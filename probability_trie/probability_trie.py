f28a1a92e41267bcae1713633f76768077b5dcbf
Simon Kuang <simontheflutist@berkeley.edu>
2019-08-19 18:18:03 -0700

    as a class

--- probability_trie/probability_trie.py
+++ probability_trie/probability_trie.py
@@ -5,54 +5,78 @@ from collections import defaultdict
 from functools import partial
 
 
-def from_text(text, n):
-    """Create a probability trie structure from this text."""
-    frequency_trie = _make_frequency_trie(text, n)
-    probability_trie = _from_frequency_trie(frequency_trie)
-    return probability_trie
+class FrequencyTrie(object):
+    def __init__(self, depth):
+        self.depth = depth
+        self.trie = FrequencyTrie._new_empty_trie(self.depth)
+
+    @staticmethod
+    def _new_empty_trie(n, end_type=int):
+        """Return a data structure such that after n-1 associative array
+        deferences, you get a defaultdict of type end_type"""
+        if n == 1:
+            return defaultdict(end_type)
+        if n > 1:
+            return defaultdict(partial(FrequencyTrie._new_empty_trie,
+                                       n - 1,
+                                       end_type))
+        raise ValueError
+
+    def update(self, text):
+        """Update frequencies using text."""
+        for ngram in self._make_ngrams(text):
+            self.update_trie(ngram)
+
+    def _make_ngrams(self, text):
+        """Return all n-grams of a specified length in a text."""
+        for i in range(len(text) - self.depth):
+            yield text[i:i + self.depth]
+
+    def update_trie(self, key):
+        def _update_trie(trie, key, func=lambda i: i + 1):
+            """Apply func to the value of this trie at key."""
+            # if we have a defaultdict of a plain type
+            if len(key) == 1:
+                trie[key] = func(trie[key])
+                return
+            if len(key) > 1:
+                _update_trie(trie[key[0]], key[1:], func)
+                return
+            raise ValueError
+        return _update_trie(self.trie, key)
+
+    @property
+    def as_dict(self):
+        def _as_dict(trie):
+            """Convert from nested defaultdict to nested dict."""
+            if not isinstance(trie, defaultdict):
+                return trie
+            return {k: _as_dict(v) for k, v in trie.items()}
+        return _as_dict(self.trie)
+
+    @property
+    def as_compressed_dict(self):
+        def _compress_trie(trie):
+            if not isinstance(trie, dict):
+                return trie
+
+            compressed_trie = dict()
+            for key, value in trie.items():
+                value = _compress_trie(value)
+                if not isinstance(value, dict):
+                    compressed_trie[key] = value
+                    continue
+                if len(value) == 1:
+                    subkey, subvalue = tuple(value.items())[0]
+                    compressed_trie[key + subkey] = subvalue
+                else:
+                    compressed_trie[key] = value
+            return compressed_trie
+        return _compress_trie(self.trie)
 
 
-def _make_ngrams(text, n):
-    """Return all n-grams of a specified length in a text."""
-    for i in range(len(text) - n):
-        yield text[i:i + n]
-
-
-def _make_empty_trie(n, end_type=int):
-    """Return a data structure such that after n-1 associative array
-    deferences, you get a defaultdict of type end_type"""
-    if n == 1:
-        return defaultdict(end_type)
-    if n > 1:
-        return defaultdict(partial(_make_empty_trie, n - 1, end_type))
-    raise ValueError
-
-
-def _update_trie(trie, key, func):
-    """Apply func to the value of trie at key."""
-    # if we have a defaultdict of a plain type
-    if len(key) == 1:
-        trie[key] = func(trie[key])
-        return
-    if len(key) > 1:
-        _update_trie(trie[key[0]], key[1:], func)
-        return
-    raise ValueError
-
-
-def _make_frequency_trie(text, n):
-    trie = _make_empty_trie(n, end_type=int)
-    for n_gram in _make_ngrams(text, n):
-        _update_trie(trie, n_gram, lambda i: i + 1)
-    return trie
-
-
-def _as_dict(trie):
-    """Convert from nested defaultdict to nested dict."""
-    if not isinstance(trie, defaultdict):
-        return trie
-    return {k: _as_dict(v) for k, v in trie.items()}
-
-
-def _from_frequency_trie(frequency_trie):
-    raise NotImplementedError
+if __name__ == '__main__':
+    import re
+    with open('data/austen.txt', 'r') as f:
+        t = f.read()
+        t = re.sub(r"""\s+""", " ", t)
